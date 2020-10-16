1f265fdc813708a6b30a19a835c835aa24bc0093
stevekriz <steve.kriz@gmail.com>
2020-10-15 23:02:53 -0700

    Implemented should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative) and (functional).

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -56,13 +56,20 @@ describe('About Applying What We Have Learnt', function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it('should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)', function () {
-    var sum = FILL_ME_IN; /* try chaining range() and reduce() */
+    var sum = _.chain(_.range(1, 1000))
+      .reduce((acc, n) => {
+        if (n % 3 === 0 || n % 5 === 0) {
+          acc += n;
+        }
+        return acc;
+      }, 0)
+      .value(); /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
