3d9b926b0aec64126a1af3362c23d4f5b1db825d
stevekriz <steve.kriz@gmail.com>
2020-10-15 19:24:30 -0700

    Implemented it should allow extra function arguments.

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -61,13 +61,13 @@ describe('About Functions', function() {
       return firstArg;
     };
 
-    expect(returnFirstArg('first', 'second', 'third')).toBe(FILL_ME_IN);
+    expect(returnFirstArg('first', 'second', 'third')).toBe('first');
 
     var returnSecondArg = function(firstArg, secondArg) {
       return secondArg;
     };
 
-    expect(returnSecondArg('only give first arg')).toBe(FILL_ME_IN);
+    expect(returnSecondArg('only give first arg')).toBe(undefined);
 
     var returnAllArgs = function() {
       var argsArray = [];
@@ -78,7 +78,7 @@ describe('About Functions', function() {
       return argsArray.join(',');
     };
 
-    expect(returnAllArgs('first', 'second', 'third')).toBe(FILL_ME_IN);
+    expect(returnAllArgs('first', 'second', 'third')).toBe('first,second,third');
   });
 
   it('should pass functions as values', function() {
